1c33ba5a0a2350540a5fa2abee70c7910048cc52
Mrbook <1985904621@qq.com>
2019-09-09 22:39:13 +0800

    0909

--- background.js
+++ background.js
@@ -1,16 +1,25 @@
 chrome.browserAction.onClicked.addListener(tab => {
     console.log('d')
 
+    let width = tab.width / 2 - 5
+    let height = tab.height + 1000
+    
     chrome.windows.create({
         type: 'popup',
-        tabId: tab.id
+        tabId: tab.id,
+        width: width,
+        height: height,
+        left: 5
     })
     chrome.windows.create({
         url: 'index.html',
         type: 'popup',
         focused: true,
-        left: 1000
+        width: width,
+        left: width + 15,
+        height: height
     })
+    console.log(width)
     chrome.tabs.executeScript(tab.id, {
         file: 'contentScript.js'
     })
